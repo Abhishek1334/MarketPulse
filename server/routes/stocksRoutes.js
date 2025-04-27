2a3f10b72f2d9ce875eec0ef6ef044f3a7f252c1
Abhishek <rajoria04ansh@gmail.com>
2025-04-27 18:54:39 +0530

    debugging4

--- server/routes/stocksRoutes.js
+++ server/routes/stocksRoutes.js
@@ -1,18 +1,18 @@
-import express from "express";
-import {
-	getStockData,
-	getStockDataforWatchlist,
-	getStockChartData,
-	searchStock
-} from "../controllers/externalStockController.js";
-const router = express.Router();
+// import express from "express";
+// import {
+// 	getStockData,
+// 	getStockDataforWatchlist,
+// 	getStockChartData,
+// 	searchStock
+// } from "../controllers/externalStockController.js";
+// const router = express.Router();
 
-router.get("/getStock", getStockData);
+// router.get("/getStock", getStockData);
 
-router.get("/getStockforWatchlist", getStockDataforWatchlist);
+// router.get("/getStockforWatchlist", getStockDataforWatchlist);
 
-router.get("/chart", getStockChartData);
+// router.get("/chart", getStockChartData);
 
-router.get("/search", searchStock);
+// router.get("/search", searchStock);
 
-export default router;
+// export default router;
