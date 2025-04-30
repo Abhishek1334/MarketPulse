4c22874488b4c752bc16fecf8493b2cb39370b4e
Abhishek <rajoria04ansh@gmail.com>
2025-05-01 01:07:04 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 const mongoURI = process.env.MONGO_URI;
 
 const corsOptions = {
-	origin: ["http://localhost:5174", "http://localhost:5173", "https://market-pulse-two.vercel.app"],
+	origin: ["https://market-pulse-two.vercel.app"],
 	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 	allowedHeaders: ["Content-Type", "Authorization"],
 	credentials: true,
